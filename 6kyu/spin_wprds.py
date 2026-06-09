30df4ba8b4af19ada0b2748953adcd9c29822efd
Samuel Macedo <185958358+smacedo7@users.noreply.github.com>
2026-06-09 12:37:46 -0300

    Organize repository structure

--- 6kyu/spin_wprds.py
+++
@@ -1,8 +0,0 @@
-def spin_words(sentence):
-    frases_splitadas = sentence.split()
-    lista = []
-    for palavra in frases_splitadas:
-        if len(palavra) >= 5:
-            palavra = palavra[::-1]
-        lista.append(palavra)
-    return ' '.join(lista)
