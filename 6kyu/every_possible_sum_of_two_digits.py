08d8e4a4fbb3ab2b734f4b1506d30265ae21879c
Samuel Macedo <185958358+smacedo7@users.noreply.github.com>
2026-06-10 23:57:20 -0300

    atualizaçao resoluçao

--- 6kyu/every_possible_sum_of_two_digits.py
+++ 6kyu/every_possible_sum_of_two_digits.py
@@ -1,6 +1,5 @@
 from math import sqrt
 
-
 def find_number(sums):
     if not sums:
         return 0
@@ -8,7 +7,15 @@ def find_number(sums):
     n = int((1 + sqrt(1 + 8 * len(sums))) / 2)
 
     if n == 2:
-        return sums[0]
+        a = min(9, sums[0])
+        b = sums[0] - a
+        return int(str(a) + str(b))
 
     a = (sums[0] + sums[1] - sums[n - 1]) // 2
-    return int(''.join(map(str, [a] + [sums[i] - a for i in range(n - 1)])))
+
+    digits = [a]
+
+    for i in range(n - 1):
+        digits.append(sums[i] - a)
+
+    return int(''.join(map(str, digits)))
