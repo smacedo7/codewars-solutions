13fe398171bfcbcbd197c01004ac8a4c061ccdeb
Samuel Macedo <185958358+smacedo7@users.noreply.github.com>
2026-06-22 19:41:01 -0300

    new resolution

---
+++ 6kyu/string.py
@@ -0,0 +1,8 @@
+def solution(s):
+    lista = []
+    for i in range(0, len(s), 2):
+        if i == len(s) - 1:
+            lista.append(s[i] + '_')
+        else:
+            lista.append(s[i:i+2])
+    return lista
