dbc75062ee4903bbf128b318fd84e36b5af8bb3b
Samuel Macedo <185958358+smacedo7@users.noreply.github.com>
2026-06-23 20:51:54 -0300

    new resolution day 23/06

--- 6kyu/spin_words.py
+++ 6kyu/spin_words.py
@@ -6,3 +6,9 @@ def spin_words(sentence):
             palavra = palavra[::-1]
         lista.append(palavra)
     return ' '.join(lista)
+
+
+# nova resoluçao dia 23/06:
+
+# def spin_words(sentence):
+#     return ' '.join([i[::-1] if len(i) >= 5 else i for i in sentence.split()])
