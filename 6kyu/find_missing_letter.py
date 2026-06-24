ab670b503d83a226283b973ebf035a8fb6221130
Samuel Macedo <185958358+smacedo7@users.noreply.github.com>
2026-06-24 18:05:23 -0300

    new resolution day 06/24

--- 6kyu/find_missing_letter.py
+++ 6kyu/find_missing_letter.py
@@ -13,3 +13,16 @@ def find_missing_letter(chars):
     # while ord(chars[n]) == ord(chars[n+1]) - 1:
     #     n += 1
     # return chr(1+ord(chars[n]))
+
+# new resolution day 06/24
+# def find_missing_letter(chars):
+#     string = ''
+#     for i in range(0, len(chars) - 1):
+#         if ord(chars[i+1]) - ord(chars[i]) != 1:
+#             string += chars[i]
+
+#     return chr(ord(string) + 1)
+
+
+# print(find_missing_letter(['a','b','c','d','f']))
+# print(find_missing_letter(['O','Q','R','S']))
