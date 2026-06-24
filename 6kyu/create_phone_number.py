37ccef0018f40768150c4d0feff613bb13db1edc
Samuel Macedo <185958358+smacedo7@users.noreply.github.com>
2026-06-23 21:32:49 -0300

    new resolution 23/06

--- 6kyu/create_phone_number.py
+++ 6kyu/create_phone_number.py
@@ -4,3 +4,12 @@ def create_phone_number(n):
     parte3 = ''.join(str(x) for x in n[6:])
 
     return f'({parte1}) {parte2}-{parte3}'
+
+
+# resoluçao dia 23/06 
+# def create_phone_number(n):
+#     string = ''
+#     for i in n:
+#         string += str(i)
+#     return f'({string[0:3]}) {string[3:6]}-{string[6:]}'
+
