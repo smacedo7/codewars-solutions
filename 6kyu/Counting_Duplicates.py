200c5ef6be11530c00c486608a9f9e2364723da0
Samuel Macedo <185958358+smacedo7@users.noreply.github.com>
2026-06-24 17:32:37 -0300

    resolution day 06/24

--- 6kyu/Counting_Duplicates.py
+++ 6kyu/Counting_Duplicates.py
@@ -13,3 +13,16 @@ def duplicate_count(text):
 # melhor solução:
 # def duplicate_count(text):
 #     return len([i for i in set(text.lower()) if text.lower().count(i) > 1])
+
+# resolution day 06/24
+
+# def duplicate_count(text):
+#     resultado = []
+#     repetido = []
+#     for i in text.upper():
+#         if i not in resultado:
+#             resultado.append(i)
+#         else:
+#             if i not in repetido:
+#                 repetido.append(i)
+#     return len(repetido)
