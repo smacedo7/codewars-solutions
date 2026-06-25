69b470ec1d910c2decb56ba8dd4e36a28b149f57
Samuel Macedo <185958358+smacedo7@users.noreply.github.com>
2026-06-24 23:54:26 -0300

    new resolution day 04/24

--- 6kyu/split_strings.py
+++ 6kyu/split_strings.py
@@ -6,3 +6,15 @@ def solution(s):
         else:
             lista.append(s[i:i+2])
     return lista
+
+
+# new resolution day 04/24
+
+# def solution(s):
+#     lista = []
+#     for i in range(0, len(s), 2):
+#         if i == len(s) - 1:
+#             lista.append(s[i] + '_')
+#         else:
+#             lista.append(s[i:i+2])
+#     return lista
