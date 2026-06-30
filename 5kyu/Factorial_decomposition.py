bda043428574abbe55fdce78d3da3e5cd0af2b8d
Samuel Macedo <185958358+smacedo7@users.noreply.github.com>
2026-06-30 00:33:02 -0300

    factorial resolution

---
+++ 5kyu/Factorial_decomposition.py
@@ -0,0 +1,41 @@
+# def decomp(n):
+#     primos = []
+#     divisores = []
+#     for i in range(1, n+1):
+#         if i == 1:
+#             continue
+#         else:
+#             for j in range(1, i+1):
+#                 if i % j == 0:
+#                     divisores.append(j)
+#             if len(divisores) == 2:
+#                 primos.append(i)
+#                 divisores.clear()
+# execuçao muito lenta.
+
+def decomp(n):
+    primos = []
+    for i in range(2, n+1):
+        é_primo = True
+        for j in range(2, int(i ** 0.5) + 1):  # ate aqui ja apareceu um divisor se tiver
+            if i % j == 0:
+                é_primo = False
+                break
+            if é_primo:
+                primos.append(i)
+    resultado = []
+
+    for primo in primos:
+        expoente = 0
+        potencia = primo
+
+        while potencia <= n:
+            expoente += n // potencia
+            potencia *= primo
+
+        if expoente == 1:
+            resultado.append(str(primo))
+        else:
+            resultado.append(f'{primo}^{expoente}')
+
+    return ' * '.join(resultado)
--- 5kyu/find_the_smallest.py
+++ 5kyu/find_the_smallest.py
@@ -1,3 +1,14 @@
 def smallest(n):
-    ...
-    
+    string = list(str(n))
+    resultados = []
+
+    for num in string:
+        copia = string.copy()
+        resultados.append(copia.pop(num))
+        
+
+
+
+numero = 261235
+print(numero)
+print(smallest(numero))
