c454cc74ad66a0dac16de9ea4dc6488f4a77f840
Samuel Macedo <185958358+smacedo7@users.noreply.github.com>
2026-06-30 01:28:59 -0300

    taka feito

--- 5kyu/Factorial_decomposition.py
+++ 5kyu/Factorial_decomposition.py
@@ -17,7 +17,8 @@ def decomp(n):
     primos = []
     for i in range(2, n+1):
         é_primo = True
-        for j in range(2, int(i ** 0.5) + 1):  # ate aqui ja apareceu um divisor se tiver
+        for j in range(2, int(i ** 0.5) + 1):  # ate aqui ja apareceu um 
+            # divisor se tiver
             if i % j == 0:
                 é_primo = False
                 break
